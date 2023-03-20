7b746c695726e45aa9e031a473e882e556004aab
CallanGeorge <callangeorge19@gmail.com>
2023-03-20 23:10:13 +0000

    final

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
   const [munros, setMunros] = useState();
   const [munro, setMunro] = useState();
   const [id, setId] = useState();
+  const [weather, setWeather] = useState();
+  const [name, setName] = useState();
+  const [height, setHeight] = useState();
+
+  const clearMunro = () => {
+    setMunro("");
+  };
 
   const getMunros = async () => {
     try {
@@ -34,6 +41,35 @@ function App() {
       console.log(singleMunro);
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const getByWeather = async (e) => {
+    e.preventDefault();
+    console.log(weather);
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/v1/munros/weather/${weather}`
+      );
+
+      const singleMunro = response.data;
+
+      setMunro(singleMunro);
+      console.log(singleMunro);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addMunro = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/v1/munros/add",
+        { name, height, weather }
+      );
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -68,13 +104,46 @@ function App() {
           Get by Id{" "}
         </button>
       </form>
-      {munro && (
-        <div>
-          <h1>{munro.name}</h1>
-          <p>{munro.weather}</p>
-          <p>{munro.height}</p>
-        </div>
-      )}
+      <form>
+        <label>
+          weather:{" "}
+          <input type="text" onChange={(e) => setWeather(e.target.value)} />
+        </label>
+        <button type="submit" onClick={(e) => getByWeather(e)}>
+          {" "}
+          Get by weather{" "}
+        </button>
+      </form>
+      <table>
+        {munro &&
+          munro.map((munro) => (
+            <tr>
+              <td>{munro.name}</td>
+              <td>{munro.weather}</td>
+              <td>{munro.height}</td>
+            </tr>
+          ))}
+      </table>
+      <button onClick={clearMunro}>Clear Munro</button>
+
+      <form>
+        <h2>add Munro</h2>
+        <label>
+          name: <input type="text" onChange={(e) => setName(e.target.value)} />
+        </label>
+        <label>
+          height:{" "}
+          <input type="number" onChange={(e) => setHeight(e.target.value)} />
+        </label>
+        <label>
+          weather:{" "}
+          <input type="text" onChange={(e) => setWeather(e.target.value)} />
+        </label>
+        <button type="submit" onClick={(e) => addMunro(e)}>
+          {" "}
+          Create Munro
+        </button>
+      </form>
     </div>
   );
 }
